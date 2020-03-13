d1a1f233f8156d6483ae7d1039102b63082c7f6f
Josh Bucklin <joshbdd@gmail.com>
2020-03-13 00:02:33 -0700

    move models to their own directory

---
+++ models/choice.go
@@ -0,0 +1,46 @@
+package models
+
+// Choice holds the id and string for a given hand in RPSLS
+type Choice struct {
+	ID   ChoiceType `json:"id"`
+	Name string     `json:"name"`
+}
+
+// GetChoices returns all choices
+func GetChoices() []Choice {
+	return []Choice{
+		Choice{Rock, Rock.String()},
+		Choice{Paper, Paper.String()},
+		Choice{Scissors, Scissors.String()},
+		Choice{Lizard, Lizard.String()},
+		Choice{Spock, Spock.String()},
+	}
+}
+
+// ChoiceType is the integer value of the chosen hand in RPSLS
+type ChoiceType int
+
+// ChoiceType constants
+const (
+	Rock ChoiceType = iota + 1
+	Paper
+	Scissors
+	Lizard
+	Spock
+)
+
+func (c ChoiceType) String() string {
+	switch c {
+	case Rock:
+		return "Rock"
+	case Paper:
+		return "Paper"
+	case Scissors:
+		return "Scissors"
+	case Lizard:
+		return "Lizard"
+	case Spock:
+		return "Spock"
+	}
+	return ""
+}
---
+++ models/match.go
@@ -0,0 +1,66 @@
+package models
+
+// Match represents a single match of RPSLS
+type Match struct {
+	Results  string     `json:"results"`
+	Player   ChoiceType `json:"player"`
+	Computer ChoiceType `json:"computer"`
+}
+
+// Win, Lose, Tie constants
+const (
+	Win  string = "win"
+	Lose string = "lose"
+	Tie  string = "tie"
+)
+
+// DetermineWinner decides the winner of a game of RPSLS
+func (m Match) DetermineWinner() {
+	switch m.Player {
+	case Rock:
+		switch m.Computer {
+		case Scissors, Lizard:
+			m.Results = Win
+		case Paper, Spock:
+			m.Results = Lose
+		default:
+			m.Results = Tie
+		}
+	case Paper:
+		switch m.Computer {
+		case Rock, Spock:
+			m.Results = Win
+		case Scissors, Lizard:
+			m.Results = Lose
+		default:
+			m.Results = Tie
+		}
+	case Scissors:
+		switch m.Computer {
+		case Paper, Lizard:
+			m.Results = Win
+		case Rock, Spock:
+			m.Results = Lose
+		default:
+			m.Results = Tie
+		}
+	case Lizard:
+		switch m.Computer {
+		case Paper, Spock:
+			m.Results = Win
+		case Rock, Scissors:
+			m.Results = Lose
+		default:
+			m.Results = Tie
+		}
+	case Spock:
+		switch m.Computer {
+		case Rock, Scissors:
+			m.Results = Win
+		case Lizard, Paper:
+			m.Results = Lose
+		default:
+			m.Results = Tie
+		}
+	}
+}
